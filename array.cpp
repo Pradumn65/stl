a2ee0586785dd0ccd95e37ec35191ced019e53de
Pradumn65 <114013210+Pradumn65@users.noreply.github.com>
2023-09-06 13:53:30 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<array>
+using namespace std;
+  
+int main()
+{
+    array<int,4> a={1,2,3,4};
+    int size =a.size();
+    for(int i=0;i<size;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"second element is: "<<a.at(2)<<endl;
+    cout<<"empty or not: "<<a.empty()<<endl;
+    cout<<"first element:  "<<a.front()<<endl;
+    cout<<"last element:  "<<a.back()<<endl;
+    return 0;
+}
